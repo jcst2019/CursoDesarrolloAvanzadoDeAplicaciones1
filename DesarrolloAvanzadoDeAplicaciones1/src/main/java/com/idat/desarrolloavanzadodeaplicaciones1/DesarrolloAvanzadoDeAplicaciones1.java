b5376be8dc1eacc70a67e35670cdd2fa87b8c237
jcst2019 <juansolar_4@hotmail.com>
2025-01-19 10:53:28 -0500

    Primer commit

---
+++ DesarrolloAvanzadoDeAplicaciones1/src/main/java/com/idat/desarrolloavanzadodeaplicaciones1/DesarrolloAvanzadoDeAplicaciones1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.idat.desarrolloavanzadodeaplicaciones1;
+
+/**
+ *
+ * @author Solaris
+ */
+public class DesarrolloAvanzadoDeAplicaciones1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
