490155cf1f8f2741c5b1c8f1bfe0c41a45252db1
jcst2019 <juansolar_4@hotmail.com>
2025-01-19 11:12:15 -0500

    VARIABLES

---
+++ DesarrolloAvanzadoDeAplicaciones1/src/main/java/com/idat/desarrolloavanzadodeaplicaciones1/VARIABLES/Variables3.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.idat.desarrolloavanzadodeaplicaciones1.VARIABLES;
+
+/**
+ *
+ * @author Solaris
+ * Este programa muestra cómo intercambiar los valores de dos variables.
+ */
+public class Variables3 {
+    
+    public static void main(String[] args) {
+        
+        // Declaramos dos variables
+        int a = 3; // Valor inicial de la variable 'a'
+        int b = 7; // Valor inicial de la variable 'b'
+
+        // Mostramos los valores iniciales
+        System.out.println("Antes del intercambio:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Intercambiamos los valores usando una variable temporal
+        int temporal = a; // Guardamos el valor de 'a' en la variable temporal
+        a = b; // Asignamos el valor de 'b' a 'a'
+        b = temporal; // Asignamos el valor original de 'a' (almacenado en temporal) a 'b'
+
+        // Mostramos los valores después del intercambio
+        System.out.println("Después del intercambio:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    
+}
